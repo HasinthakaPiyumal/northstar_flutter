5e4c9901ad777fb7400b736541a6b62d269dd82f
HasinthakaPiyumal <hasinthakapiyumal@gmail.com>
2024-08-15 20:26:34 +0530

    zego updated

--- lib/Controllers/ZegoCloudController.dart
+++ lib/Controllers/ZegoCloudController.dart
@@ -1,6 +1,6 @@
 class ZegoCloudController{
-  static int appID = 1190971533;
-  static String appSign = "e20b088ed3b449d8aa0e5f7758563d5050e852c006bc05decfe7e2746da15639";
+  static int appID = 1797664492;
+  static String appSign = "990e19df9f0d2c5d70ffa47a4a8acb441405c0be24f12835715157408ab7d1ee";
   static DateTime timeStamp = DateTime.now();
   static bool isOutgoing = false;
   static int calleeId = 0;
