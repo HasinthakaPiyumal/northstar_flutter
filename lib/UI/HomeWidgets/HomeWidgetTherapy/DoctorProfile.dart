467118212a3963223f487a98cade533bd71ce7dc
HasinthakaPiyumal <hasinthakapiyumal@gmail.com>
2024-11-10 21:44:28 +0530

    carousol fix

--- lib/UI/HomeWidgets/HomeWidgetTherapy/DoctorProfile.dart
+++ lib/UI/HomeWidgets/HomeWidgetTherapy/DoctorProfile.dart
@@ -1,5 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 import 'package:carousel_slider/carousel_slider.dart';
+
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
